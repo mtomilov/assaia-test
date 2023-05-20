27709c2c39ec169b0671f1c1a2e597255531e6d2
Michael Tomilov <mtomilov@gmail.com>
2023-05-20 13:32:01 +0400

    Add connect tests

---
+++ connect/test_connect.py
@@ -0,0 +1,32 @@
+import pytest
+
+from .connect import CONNECTIONS, Connect, InvalidMove, Symbol
+
+# TODO: add tests
+
+
+@pytest.mark.parametrize("player", list(Symbol))
+def test_win(player: Symbol) -> None:
+    connect = Connect()
+    for _ in range(CONNECTIONS):
+        assert not connect.won()
+        connect.drop(player, 0)
+    assert connect.won()
+
+
+@pytest.mark.parametrize("player", list(Symbol))
+def test_not_win(player: Symbol) -> None:
+    connect = Connect()
+    for _ in range(CONNECTIONS - 1):
+        assert not connect.won()
+        connect.drop(player, 0)
+    assert not connect.won()
+
+
+@pytest.mark.parametrize("player", list(Symbol))
+def test_invalid_move(player: Symbol) -> None:
+    connect = Connect()
+    with pytest.raises(InvalidMove):
+        connect.drop(player, -1)
+    with pytest.raises(InvalidMove):
+        connect.drop(player, connect.width + 1)
